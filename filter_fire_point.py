8d9c9d03b13bacb5bfcac44efddbf4e4d0cfd8c4
weiyu <weiyu_zhang@pku.edu.cn>
2023-05-29 11:34:46 +0800

    Update filter_fire_point.py

--- filter_fire_point.py
+++ filter_fire_point.py
@@ -4,9 +4,8 @@ import pandas as pd
 import rasterio.features
 import rasterio.mask
 
-import fiona
 import geopandas as gpd
-import shapely.geometry
+from shapely.geometry import Point
 
 
 # Open the raster file
@@ -16,31 +15,28 @@ with rasterio.open('path/to/raster.tif') as src:
     # Get the transform information
     transform = src.transform
 
-    # Open the shapefile and read its features into a DataFrame
-    with fiona.open('path/to/shapefile.shp') as shp:
-        df = pd.DataFrame(shp)
+    # Read the point coordinates from the CSV file into a DataFrame
+    coords_df = pd.read_csv('path/to/coords.csv')
 
-    # Extract the point coordinates from the 'geometry' column in the DataFrame
-    df['geometry'] = df['geometry'].apply(shapely.geometry.shape)
-    df['x'] = df['geometry'].apply(lambda x: x.coords[0][0])
-    df['y'] = df['geometry'].apply(lambda x: x.coords[0][1])
+    # Create a Point GeoDataFrame from the coordinates
+    geometry = [Point(xy) for xy in zip(coords_df['lon'], coords_df['lat'])]
+    gdf = gpd.GeoDataFrame(coords_df, crs='EPSG:4326', geometry=geometry)
+    # Transform the GeoDataFrame to the same CRS as the raster data
+    gdf = gdf.to_crs(src.crs)
 
-    # Transform the coordinates to pixel values
-    df['px'] = ((df['x'] - transform[0]) / transform[1]).astype(int)
-    df['py'] = ((df['y'] - transform[3]) / transform[5]).astype(int)
+    # Transform the point coordinates to pixel values
+    gdf['px'] = ((gdf['geometry'].x - transform[0]) / transform[1]).astype(int)
+    gdf['py'] = ((gdf['geometry'].y - transform[3]) / transform[5]).astype(int)
 
     # Get the values of the pixels at the positions of the points
-    pixel_vals = raster_data[df['py'], df['px']]
+    pixel_vals = raster_data[gdf['py'], gdf['px']]
 
-    # Find the features with points at pixels with value 1
-    df_filtered = df[pixel_vals == 1]
+    # Find the points with pixel values of 1
+    gdf_filtered = gdf[pixel_vals == 1]
 
-    # Convert the DataFrame to a GeoDataFrame
-    gdf_filtered = gpd.GeoDataFrame(df_filtered, crs=shp.crs)
-
-    # Save the filtered features to a GeoPackage file
-    gdf_filtered.to_file('path/to/filtered_features.gpkg', driver='GPKG')
+    # Save the filtered points to a GeoPackage file
+    gdf_filtered.to_file('path/to/filtered_points.gpkg', driver='GPKG')
 
     # Print the results
-    print(f"Found {len(df_filtered)} features located at pixels with value 1.")
-    print(gdf_filtered[['x', 'y']].values)
+    print(f"Found {len(gdf_filtered)} points located at pixels with value 1.")
+    print(gdf_filtered[['lon', 'lat']].values)
